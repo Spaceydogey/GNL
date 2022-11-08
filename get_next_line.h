6221367ce8bde14aa01653364b47d61ab88522b5
Hugo Delmas <hdelmas@shi-r10-p2.s19.be>
2022-11-08 13:43:13 +0100

    join_

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 10:28:41 by hdelmas           #+#    #+#             */
-/*   Updated: 2022/10/31 17:23:31 by hdelmas          ###   ########.fr       */
+/*   Updated: 2022/11/08 13:42:57 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@
 
 int		ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
-char	*ft_strdup(char *s, int *check);
+char	*join_strdup(char *s, int *check);
 void	ft_bzero(void *s, size_t n);
 int		ft_len_to_add(char *str);
 char	*add_to_line(char *res, char *remains, int *check, int fd);
-char	*ft_strjoin(char *s1, char *s2, int *check);
+char	*join_strjoin(char *s1, char *s2, int *check);
 char	*get_next_line(int fd);
 
 #endif
